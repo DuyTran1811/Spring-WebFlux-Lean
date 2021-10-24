c502e287ccf1ed8660c5982bba160864a2e17cda
DuyTC <duytc@DuyTCs-MacBook-Pro.local>
2021-10-24 12:36:14 +0700

    Spting WebFlux

---
+++ web-flux-demo/src/main/java/com/example/webfluxdemo/exceptionhandler/InputValidationHandler.java
@@ -0,0 +1,19 @@
+package com.example.webfluxdemo.exceptionhandler;
+
+import com.example.webfluxdemo.dto.InputFailedValidationResponse;
+import com.example.webfluxdemo.exception.InputValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class InputValidationHandler {
+    @ExceptionHandler(InputValidationException.class)
+    public ResponseEntity<InputFailedValidationResponse> handlerException(InputValidationException exception) {
+        InputFailedValidationResponse response = new InputFailedValidationResponse();
+        response.setErrorCode(exception.getErrorCode());
+        response.setInput(exception.getInput());
+        response.setMessage(exception.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+}
